9ba4d5b3ce86ab45d61e81d30ecb35bca0646ee5
Marc Thiongo <marcthuo@gmail.com>
2019-04-02 14:12:59 -0400

    first commit

---
+++ notes.md
@@ -0,0 +1,11 @@
+so why rest?
+
+| Not using RESt     |  Using REST               |
+| -------------------| --------------------------|
+|
+
+
+
+
+
+https://www.google.com/search?q=query+string+paremaeters&oq=query
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+// import router
+const hubsRouter = require('./hubs/hubs-router.js');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/hubs', hubsRouter) //Delegates requests to /api/hubs to the router.
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+
+
+module.exports = server // CommonJS way of exporting out of a module.
+
+// Same as export default server
